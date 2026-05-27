13358f25ba61300655de3fb58d8c04e90325a863
soulbladereactfirebase <ybelov276@gmail.com>
2026-05-27 18:59:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const sdk = require('node-appwrite');
 const admin = require('firebase-admin');
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = async function (req, res) {
   try {
-    const token = 'en5FPEPUTISW216GM8-9qb:APA91bFX9aF IXWVGV9DN064iCww-qRo38udJHQ8D8Yq bqlNzF8T296PpHEgBiNMCHUakJfU2nqeld jnv2wmeFnlZu9ojHqHvMf45yZ5n0x8RqV1 HEulzSIE';
-
     const message = {
       notification: {
         title: 'Тест',
         body: 'Firebase работает',
       },
-      token: token,
+      token: 'en5FPEPUTISW216GM8-9qb:APA91bFX9aF IXWVGV9DN064iCww-qRo38udJHQ8D8Yq bqlNzF8T296PpHEgBiNMCHUakJfU2nqeld jnv2wmeFnlZu9ojHqHvMf45yZ5n0x8RqV1 HEulzSIE',
     };
 
     const response = await admin.messaging().send(message);
 
-    return res.json({
+    return JSON.stringify({
       success: true,
       response,
     });
   } catch (error) {
-    return res.json({
+    return JSON.stringify({
       success: false,
       error: error.message,
+      stack: error.stack,
     });
   }
 };
