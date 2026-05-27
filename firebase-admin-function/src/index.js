3ba51d020c7f680ed5423698b1c6a3badc71c7ef
soulbladereactfirebase <ybelov276@gmail.com>
2026-05-27 19:10:57 +0300

    Add files via upload

---
+++ firebase-admin-function/src/index.js
@@ -0,0 +1,34 @@
+const admin = require('firebase-admin');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+module.exports = async function (req, res) {
+  try {
+    const message = {
+      notification: {
+        title: 'Тест',
+        body: 'Firebase работает',
+      },
+      token: 'en5FPEPUTISW216GM8-9qb:APA91bFX9aF IXWVGV9DN064iCww-qRo38udJHQ8D8Yq bqlNzF8T296PpHEgBiNMCHUakJfU2nqeld jnv2wmeFnlZu9ojHqHvMf45yZ5n0x8RqV1 HEulzSIE',
+    };
+
+    const response = await admin.messaging().send(message);
+
+    return JSON.stringify({
+      success: true,
+      response,
+    });
+  } catch (error) {
+    return JSON.stringify({
+      success: false,
+      error: error.message,
+      stack: error.stack,
+    });
+  }
+};
